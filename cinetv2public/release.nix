28074c66a9732aed447f001cd8e55ce8dc6054f0
kolam <klambroulatreille@cinematheque.qc.ca>
2020-12-16 16:43:30 -0500

    Version spécifique de nixpkgs mis dans un fichier à part

--- cinetv2public/release.nix
+++ cinetv2public/release.nix
@@ -1,14 +1,5 @@
 let
-  pkgs = import (builtins.fetchGit {
-    # Descriptive name to make the store path easier to identify
-    name = "nixos-unstable-2020-04-29";
-    url = "https://github.com/nixos/nixpkgs-channels/";
-    # Commit hash for nixos-unstable as of 2018-09-12
-    # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-    ref = "refs/heads/nixos-unstable";
-    rev = "7c399a4ee080f33cc500a3fda33af6fccfd617bd";
-  }) {};
-  # pkgs = import ./nixpkgs.nix;
+  pkgs = import ./nixpkgs.nix;
 
   haskellPackages = pkgs.haskell.packages.ghc883;
 
@@ -25,19 +16,3 @@ in
     docopt = pkgs.haskell.lib.dontCheck docoptPkgs;
     cinetv4h = haskellPackages.callCabal2nix "cinetv4h" (./../cinetv4h) {};
   }
-  # pkgs.callPackage (./.) {
-  #   inherit mkDerivation;
-  #   # inherit (pkgs) stdenv;
-  #   inherit (haskellPackages) base;
-  #   cinetv4h = haskellPackages.callCabal2nix "cinetv4h" (./../cinetv4h) {};
-  #   inherit (haskellPackages) text;
-  #   inherit (haskellPackages) directory;
-  #   docopt = pkgs.haskell.lib.dontCheck docoptPkgs;
-  #   inherit (haskellPackages) esqueleto;
-  #   inherit (haskellPackages) filepath;
-  #   inherit (haskellPackages) hpack;
-  #   inherit (haskellPackages) persistent-sqlite;
-  #   inherit (haskellPackages) regex;
-  #   inherit (haskellPackages) resource-pool;
-  #   inherit (haskellPackages) unliftio;
-  # }
--- cinetv2public/shell.nix
+++ cinetv2public/shell.nix
@@ -1,21 +1,7 @@
 let
-  pkgs = import (builtins.fetchGit {
-    # Descriptive name to make the store path easier to identify
-    name = "nixos-unstable-2020-04-29";
-    url = "https://github.com/nixos/nixpkgs-channels/";
-    # Commit hash for nixos-unstable as of 2018-09-12
-    # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-    ref = "refs/heads/nixos-unstable";
-    rev = "7c399a4ee080f33cc500a3fda33af6fccfd617bd";
-  }) {};
+  pkgs = import ./nixpkgs.nix;
 
   inherit (pkgs) haskellPackages;
-
-  # haskellPackages = pkgs.haskell.packages.${compiler};
-  # haskellPackages.callCabal2nix "cinetv2public" (./.) {
-  #   cinetv4h = haskellPackages.callCabal2nix "cinetv4h" (./../cinetv4h) {};
-  # }
-  # callCabal2nix = pkgs.haskell.packages.ghc883.callCabal2nix;
 
   project = import ./release.nix;
 in
